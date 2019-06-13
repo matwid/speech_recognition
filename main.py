9f7d637d5f2bb3f239508fc813a316d649ac3992
matwid <m.widmann@physik.uni-stuttgart.de>
2019-06-13 15:18:11 +0200

    renaming methods

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 #  Zhang, A. (2017). Speech Recognition (Version 3.8) [Software]. Available from https://github.com/Uberi/speech_recognition#readme.
 #  Licensing information for SpeechRecognition can be found within the SpeechRecognition README
 
-import speech_recognition as sr
+import speech_recognition
 
-recognizer = sr.Recognizer()
+recognizer = speech_recognition.Recognizer()
 
-with sr.Microphone() as source:
+with speech_recognition.Microphone() as source:
     print("Talk now")
     audio = recognizer.listen(source)
     try:
-        text = r.recognize_google(audio)
+        text = recognizer.recognize_google(audio)
         print("You just said : {}".format(text))
     except:
         print("No voice recognized")
